98e247a0ef5b54e8377cd5c0af7ffbad5fc84117
sonht1109 <yourgitemail@example.com>
2026-09-18 10:57:10 +0700

    fix: claude notify bash should support linux

--- .claude/scripts/hooks/notify.sh
+++ .claude/scripts/hooks/notify.sh
@@ -3,7 +3,13 @@
 
 BASH_SOURCE_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-which terminal-notifier >/dev/null 2>&1 && terminal-notifier "$@" \
-  -sound default \
-  -appIcon "file://$BASH_SOURCE_DIR/claude-logo.svg" \
-  -sender "com.apple.Terminal"
+if command -v terminal-notifier >/dev/null 2>&1; then
+  terminal-notifier "$@" \
+    -sound default \
+    -appIcon "file://$BASH_SOURCE_DIR/claude-logo.svg" \
+    -sender "com.apple.Terminal"
+elif command -v notify-send >/dev/null 2>&1; then
+  notify-send "$@"
+fi
+
+exit 0
